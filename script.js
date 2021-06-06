4cd0c7d421e14cf60bf803d9185ae337ab01ad83
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-06-06 14:59:41 -0500

    submit button and saving to local storage

--- script.js
+++ script.js
@@ -21,50 +21,54 @@ const question = [
 var i=0;
 var score = 0;
 const wrongAnswer = 5;
-var timeLeft = 75;
+var timeLeft = 15;
+//score
+let inputInitials = document.getElementById("results");
 //TO DO: Remove starting buttons
+
+
 function start(){
+  document.getElementById('optionbutton').className="";
   //remove start button  
-    content.innerHTML = "";
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function() {
-     if (timeLeft<=0){ //if timer reaches 0 gameover
+  content.innerHTML = "";
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function() {
+    //if timer reaches 0 gameover
+    if (timeLeft<=0){ 
       console.log(timeLeft)
        clearInterval(timeInterval);
        timer.innerHTML= "Game Over";
        gameEnd(); 
-     }
-     else {
-        timer.innerHTML= timeLeft + " seconds";
-        //start quiz
-        var displayQuestion = question[i].q;      
-        questions.innerHTML = displayQuestion;
-        //display options
-        var displayOption = question[i].option;
-        //will display options on each div accordingly (options0,options1..)
-        for (var q=0; q<displayOption.length; q++){
-          document.getElementById("options"+q).innerHTML=displayOption[q];
-
-        }
-        //create an eventListener
-        document.getElementById("option")
-        //once element is clicked then it will move to next question
-     }
-     timeLeft--;
-    },1000)
+    }
+    else {
+      timer.innerHTML= timeLeft + " seconds";
+      var displayQuestion = question[i].q;      
+      questions.innerHTML = displayQuestion;
+      //display options
+      var displayOption = question[i].option;
+      //will display options on each div accordingly (options0,options1..)
+      for (var q=0; q<displayOption.length; q++){
+        document.getElementById("options"+q).innerHTML=displayOption[q];
+      }
+      //create an eventListener
+      document.getElementById("option")
+      //once element is clicked then it will move to next question
+    }
+    timeLeft--;
+  },1000)
 }
 
 function nextquestion(answer) {  
   //if selected option is correct then add X to score
   if (answer==question[i].correct){
     alert("correct");
-    score ++;
-    console.log (score);
+    score++;
+    //console.log (score);
   }
-   //else -X to score && -5seconds to timerLeft
+  //else -X to score && -5seconds to timerLeft
   else {
     alert("incorrect");
-    score --;
+    score--;
     timeLeft = (timeLeft-wrongAnswer);
     console.log(timeLeft);
   }
@@ -74,5 +78,24 @@ function nextquestion(answer) {
 }
 
 function gameEnd (){
+  var element =document.getElementById("quizcontainer");
+  element.className="hide";
+  document.getElementById('results').className="";
+};
 
-}
+document.getElementById('submit').addEventListener('click', function(event) {
+  event.preventDefault();
+
+  var userInitials = document.querySelector('#userinitials').value;
+  
+  if (userInitials === '') {
+    alert("Initials cannot be blank");
+  } 
+  else {
+    alert('Initials submitted successfully');
+
+    // Save email and password to localStorage using `setItem()`
+    localStorage.setItem('userinitials', userInitials);
+    // Render the last registered email and password
+  }
+});
